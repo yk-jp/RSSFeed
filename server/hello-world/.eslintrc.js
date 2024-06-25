fb286c88581c4c05f1dabc9733edbcaff7498509
yk-jp <k82.y12@gmail.com>
2024-06-24 21:56:47 -0700

    Clean the server code.

--- server/hello-world/.eslintrc.js
+++ server/hello-world/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
       ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-      sourceType: "module" 
+      sourceType: "module"
     },
     extends: [
       "plugin:@typescript-eslint/recommended", // recommended rules from the @typescript-eslint/eslint-plugin
