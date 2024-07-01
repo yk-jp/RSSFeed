d09fcccdc188cbe73adfb3d54b69bb7593e271a2
yk-jp <k82.y12@gmail.com>
2024-07-01 14:38:27 -0700

    Add types to scraping logic.

--- server/api/theconversation/app.ts
+++ server/api/theconversation/app.ts
@@ -3,25 +3,37 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 import { THECONVERSATION_URL_CA } from './config';
+import { News } from './types';
 
-const fetchNews = async (): Promise<any> => {
+const fetchNews = async (): Promise<News[]> => {
     const response = await axios.get(THECONVERSATION_URL_CA);
     const $ = cheerio.load(response.data);
-    const res = [];
+    const res: News[] = [];
 
     const sections = $('[data-testid="grid"]');
-    const first7Sections = sections.slice(0, 7).get();
+    const hotTopicSections = sections.slice(0, 7).get();
 
-    first7Sections.forEach((section) => {
+    hotTopicSections.forEach((section) => {
         const newsDivs = $(section).find(`div[data-testid="ImmutableGridSlot"]`);
 
-        newsDivs.each((index, news) => {
+        newsDivs.each((_, news) => {
+            const link = $(news).find('a').attr('href');
+
+            if (!link) {
+                return;
+            }
+
             let title = $(news).find('h3 span').text();
+            let image = $(news).find('img').attr('src');
+
             if (!title) {
                 title = $(news).find('h5 span').text();
             }
-            const link = $(news).find('a').attr('href');
-            const image = $(news).find('img').attr('src');
+
+            if (!image) {
+                image = '';
+            }
+
             res.push({ title, link, image });
         });
     });
@@ -30,7 +42,7 @@ const fetchNews = async (): Promise<any> => {
 };
 
 export const lambdaHandler: Handler = async (): Promise<void> => {
-    const news = await fetchNews();
+    const news: News[] = await fetchNews();
     console.log(news);
     console.log(news.length);
 };
---
+++ server/api/theconversation/types.ts
@@ -0,0 +1,5 @@
+export type News = {
+    title: string;
+    link: string;
+    image: string;
+};
