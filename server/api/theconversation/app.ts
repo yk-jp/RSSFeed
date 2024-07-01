7e59b7952e1a27c7bb2bb0f9c0ce9242bf66ad50
yk-jp <k82.y12@gmail.com>
2024-07-01 11:06:23 -0700

    Add template of fetch function for the conversation.
    - Timeout needs to be revised.
    - Refoctoring of the function is also necessary.

--- server/api/theconversation/app.ts
+++ server/api/theconversation/app.ts
@@ -1,17 +1,36 @@
 import { Handler } from 'aws-lambda';
-import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 import { THECONVERSATION_URL_CA } from './config';
 
+const fetchNews = async (): Promise<any> => {
+    const response = await axios.get(THECONVERSATION_URL_CA);
+    const $ = cheerio.load(response.data);
+    const res = [];
+
+    const sections = $('[data-testid="grid"]');
+    const first7Sections = sections.slice(0, 7).get();
+
+    first7Sections.forEach((section) => {
+        const newsDivs = $(section).find(`div[data-testid="ImmutableGridSlot"]`);
+
+        newsDivs.each((index, news) => {
+            let title = $(news).find('h3 span').text();
+            if (!title) {
+                title = $(news).find('h5 span').text();
+            }
+            const link = $(news).find('a').attr('href');
+            const image = $(news).find('img').attr('src');
+            res.push({ title, link, image });
+        });
+    });
+
+    return res;
+};
+
 export const lambdaHandler: Handler = async (): Promise<void> => {
-    const browser = await puppeteer.launch({ headless: 'shell' });
-    const page = await browser.newPage();
-
-    await page.goto(THECONVERSATION_URL_CA);
-    await page.screenshot({ path: 'example.png' });
-    const html = await page.content();
-    console.log(html);
-
-    await browser.close();
+    const news = await fetchNews();
+    console.log(news);
+    console.log(news.length);
 };
