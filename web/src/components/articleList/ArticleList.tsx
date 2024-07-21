7beb4a3417fb3b464c8b3df72341fdf88995ded6
yk-jp <k82.y12@gmail.com>
2024-07-21 16:59:45 -0700

    Add Link to article list to make it more realistic for the component.

--- web/src/components/articleList/ArticleList.tsx
+++ web/src/components/articleList/ArticleList.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import styles from "./articleList.module.css";
 
 import Article from "../article/Article";
@@ -12,50 +14,109 @@ import lakeImg from "@/components/assets/dummy/lake.jpg";
 import lizardImg from "@/components/assets/dummy/lizard.jpg";
 import seaImg from "@/components/assets/dummy/sea.jpg";
 
-const images = [
-  birdImg,
-  boatImg,
-  bokehImg,
-  moonImg,
-  girlImg,
-  githubImg,
-  lakeImg,
-  lizardImg,
-  seaImg,
-  moonImg,
-  githubImg,
-  girlImg,
-  githubImg,
-  bokehImg,
-  boatImg,
-  bokehImg,
-  lizardImg,
-  seaImg,
+const newsData = [
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: birdImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: boatImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: bokehImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: moonImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: girlImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: githubImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: lakeImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: lizardImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: seaImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: moonImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: githubImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: girlImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: githubImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: bokehImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: boatImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    image: bokehImg,
+    link: "https://theconversation.com/biden-steps-aside-setting-in-motion-an-unprecedented-period-in-american-politics-235189",
+  },
 ];
-
 const ArticleList = () => {
   return (
     <div className={styles.container}>
       <div className={styles.gridContainer}>
-        {images.map((imgSrc, index) => (
-          <div
+        {newsData.map((news, index) => (
+          <Link
+            href={news.link}
             key={index}
-            style={{
-              backgroundImage: `url(${imgSrc.src})`,
-            }}
             className={`${styles.wrapper} ${
               index === 0 ? styles.newsTall : index === 1 ? styles.newsWide : ""
             }`}
           >
-            <h5 className={styles.newsTitle}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </h5>
-          </div>
+            <div
+              className={styles.imageContainer}
+              style={{
+                backgroundImage: `url(${news.image.src})`,
+              }}
+            >
+              <h5 className={styles.newsTitle}>{news.title}</h5>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
